44e959aa0e44b35feb3d057d5a50fbc18982f52c
Alex Glaubitz <e.pirazzi@gmail.com>
2022-07-03 14:31:10 -0400

    basic components created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Project from "./components/Project";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div>hello</div>
+    <div>
+      <Header></Header>
+      <Footer></Footer>
+    </div>
   );
 }
 
