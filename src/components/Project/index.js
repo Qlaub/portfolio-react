5a71a000fe4a33c1bfb865e7adec5f49e7d0fbe4
Alex Glaubitz <e.pirazzi@gmail.com>
2022-07-03 16:58:09 -0400

    images added to assets

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -6,37 +6,43 @@ function Project() {
       name: "PotLuck",
       techUsed: "MongoDB/Express/Node.js/Handlebars",
       deployUrl: "https://potluck-inc.herokuapp.com/",
-      repoUrl: "https://github.com/Qlaub/potluck"
+      repoUrl: "https://github.com/Qlaub/potluck",
+      imageName: "potluck"
     },
     {
       name: "Color Coded",
       techUsed: "JS/CSS/HTML",
       deployUrl: "https://qlaub.github.io/Color-Coded/",
-      repoUrl: "https://github.com/Qlaub/Color-Coded"
+      repoUrl: "https://github.com/Qlaub/Color-Coded",
+      imageName: "colorcoded"
     },
     {
       name: "Code Quiz",
       techUsed: "JS/CSS/HTML",
       deployUrl: "https://qlaub.github.io/Code-Quiz/",
-      repoUrl: "https://github.com/Qlaub/Code-Quiz"
+      repoUrl: "https://github.com/Qlaub/Code-Quiz",
+      imageName: "codequiz"
     },
     {
       name: "Weather Dashboard",
       techUsed: "JS/CSS/HTML",
       deployUrl: "https://qlaub.github.io/Weather-Dashboard/",
-      repoUrl: "https://github.com/Qlaub/Weather-Dashboard"
+      repoUrl: "https://github.com/Qlaub/Weather-Dashboard",
+      imageName: "weather"
     },
     {
       name: "Work Day Scheduler",
       techUsed: "JS/CSS/HTML",
       deployUrl: "https://qlaub.github.io/Work-Day-Scheduler/",
-      repoUrl: "https://github.com/Qlaub/Work-Day-Scheduler"
+      repoUrl: "https://github.com/Qlaub/Work-Day-Scheduler",
+      imageName: "scheduler"
     },
     {
       name: "Password Generator",
       techUsed: "JS/CSS/HTML",
       deployUrl: "https://qlaub.github.io/Password-Generator/",
-      repoUrl: "https://github.com/Qlaub/Password-Generator"
+      repoUrl: "https://github.com/Qlaub/Password-Generator",
+      imageName: "password"
     },
   ];
 
