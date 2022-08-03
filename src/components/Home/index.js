ad67c23d8042e971b6f1bb2e3dacf4a2e0a3519d
Alex Glaubitz <e.pirazzi@gmail.com>
2022-08-03 14:33:21 -0400

    updated fonts and styling in header/nav/home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,14 @@ import React from "react";
 
 function Home({linkSelected, setLinkSelected}) {
   return (
-    <div>Home</div>
+    <div className="flex justify-center items-center h-96 bg-transparent">
+      <div className="bg-quaternary p-10 mt-8 rounded h-fit">
+        <h2 className="text-xl">Hello! I'm Alex Glaubitz</h2>
+        <p>
+          This page is underway but please feel free to check out the rest of my portfolio!
+        </p>
+      </div>
+    </div>
   )
 }
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -23,29 +23,29 @@ function Nav({linkSelected, setLinkSelected}) {
 
   return (
 <nav>
-  <ul className="flex ml-6">
+  <ul className={`flex ml-6 rounded bg-quaternary`}>
     <Link to="/" className=""> 
-      <li className={`mr-5 md:mr-8 py-2 px-3 text-lg text-tertiary tracking-wide transition ease-in-out duration-75 hover:border-lighter cursor-pointer border border-transparent rounded-sm ${linkSelected === 'home' && 'bg-secondary'}`} 
+      <li className={`py-2 px-8 text-lg text-tertiary tracking-wide transition ease-in-out duration-75 hover:border-lighter cursor-pointer border border-transparent rounded-sm ${linkSelected === 'home' && 'bg-secondary'}`} 
         onClick={() => clickHandler('home')}>Home
       </li>
     </Link>
     <Link to="/about"> 
-      <li className={`mr-5 md:mr-8 py-2 px-3 text-lg text-tertiary tracking-wide transition ease-in-out duration-75 hover:border-lighter cursor-pointer border border-transparent rounded-sm ${linkSelected === 'about' && 'bg-secondary'}`} 
+      <li className={`py-2 px-8 text-lg text-tertiary tracking-wide transition ease-in-out duration-75 hover:border-lighter cursor-pointer border border-transparent rounded-sm ${linkSelected === 'about' && 'bg-secondary'}`} 
         onClick={() => clickHandler('about')}>About
       </li>
     </Link>
     <Link to="/portfolio"> 
-      <li className={`mr-5 md:mr-8 py-2 px-3 text-lg text-tertiary tracking-wide transition ease-in-out duration-75 border border-transparent hover:border-lighter cursor-pointer rounded-sm ${linkSelected === 'work' && 'bg-secondary'}`}  
+      <li className={`py-2 px-8 text-lg text-tertiary tracking-wide transition ease-in-out duration-75 border border-transparent hover:border-lighter cursor-pointer rounded-sm ${linkSelected === 'work' && 'bg-secondary'}`}  
         onClick={() => clickHandler('work')}>Work
       </li>
     </Link>
     <Link to="/contact"> 
-      <li className={`mr-5 md:mr-8 py-2 px-3 text-lg text-tertiary tracking-wide transition ease-in-out duration-75 border border-transparent hover:border-lighter cursor-pointer rounded-sm ${linkSelected === 'contact' && 'bg-secondary'}`} 
+      <li className={`py-2 px-8 text-lg text-tertiary tracking-wide transition ease-in-out duration-75 border border-transparent hover:border-lighter cursor-pointer rounded-sm ${linkSelected === 'contact' && 'bg-secondary'}`} 
         onClick={() => clickHandler('contact')}>Contact
       </li>
     </Link>
     <Link to="/resume"> 
-      <li className={`mr-5 md:mr-8 py-2 px-3 text-lg text-tertiary tracking-wide transition ease-in-out duration-75 border border-transparent hover:border-lighter cursor-pointer rounded-sm ${linkSelected === 'resume' && 'bg-secondary'}`}  
+      <li className={`py-2 px-8 text-lg text-tertiary tracking-wide transition ease-in-out duration-75 border border-transparent hover:border-lighter cursor-pointer rounded-sm ${linkSelected === 'resume' && 'bg-secondary'}`}  
         onClick={() => clickHandler('resume')}>Resume
       </li>
     </Link>
