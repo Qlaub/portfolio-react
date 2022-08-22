cc0a2cbab0a03bfd0f8059a83809809c756e0d62
Alex Glaubitz <e.pirazzi@gmail.com>
2022-08-22 18:25:08 -0400

    descriptions to projects, picture to left details to right

--- src/components/Carousel/data.js
+++ src/components/Carousel/data.js
@@ -11,42 +11,48 @@ export const slideData = [
     techUsed: "MongoDB/Express/React/Node",
     deployUrl: "https://pacific-depths-60618.herokuapp.com/",
     repoUrl: "https://github.com/beatricekallen/roam",
-    image: roam
+    image: roam,
+    description: "A travel application with an emphasis on sustainability"
   },
   {
     name: "PotLuck",
     techUsed: "MongoDB/Express/Node",
     deployUrl: "https://potluck-inc.herokuapp.com/",
     repoUrl: "https://github.com/Qlaub/potluck",
-    image: potluck
+    image: potluck,
+    description: "A pay-it-forward ordering application that takes community to the next level"
   },
   {
     name: "Color Coded",
     techUsed: "JS/CSS/HTML",
     deployUrl: "https://qlaub.github.io/Color-Coded/",
     repoUrl: "https://github.com/Qlaub/Color-Coded",
-    image: colorcoded
+    image: colorcoded,
+    description: "A website design tool to test color palettes and designs"
   },
   {
     name: "Code Quiz",
     techUsed: "JS/CSS/HTML",
     deployUrl: "https://qlaub.github.io/Code-Quiz/",
     repoUrl: "https://github.com/Qlaub/Code-Quiz",
-    image: codequiz
+    image: codequiz,
+    description: "A simple quiz website that tests you on HTML/CSS/JS questions"
   },
   {
     name: "Weather Dashboard",
     techUsed: "JS/CSS/HTML",
     deployUrl: "https://qlaub.github.io/Weather-Dashboard/",
     repoUrl: "https://github.com/Qlaub/Weather-Dashboard",
-    image: weather
+    image: weather,
+    description: "Want to know the weather conditions for a particular place? Check it out here!"
   },
   {
     name: "Work Day Scheduler",
     techUsed: "JS/CSS/HTML",
     deployUrl: "https://qlaub.github.io/Work-Day-Scheduler/",
     repoUrl: "https://github.com/Qlaub/Work-Day-Scheduler",
-    image: scheduler
+    image: scheduler,
+    description: "Plan your day with this simple day calendar"
   },
   // {
   //   name: "Password Generator",
--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -19,7 +19,7 @@ const Carousel = () => {
   }
 
   return (
-    <section className={'flex justify-center items-center relative lg:h-screen2'} >
+    <section className='flex justify-center items-center relative lg:h-screen2' >
       <FaArrowAltCircleLeft className='right-arrow absolute top-1/2 text-black z-10 cursor-pointer select-none lg:left-12 lg:text-5xl' onClick={prevSlide} />
       <FaArrowAltCircleRight className='absolute top-1/2 text-black z-10 cursor-pointer select-none lg:right-12 lg:text-5xl' onClick={nextSlide} />
       {slideData.map((slide, index) => {
@@ -30,10 +30,15 @@ const Carousel = () => {
           >
             {index === currentSlide && (
               <>
-                <img src={slide.image} alt='travel image' className='rounded-md lg:h-screen2 ' />
-                <p>{slide.name}</p>
-                <p>{slide.deployUrl}</p>
-                <p>{slide.repoUrl}</p>
+                <img src={slide.image} alt='travel image' className='rounded-md lg:h-screen2 w-2/3' />
+                <div className='absolute top-0 right-0 text-right'>
+                  <h2 className='text-4xl m-2'>{slide.name}</h2>
+                  <div className='bg-white rounded p-4 mt-6'>
+                    <p>{slide.description}</p>
+                  </div>
+                  <p className='mt-6'>{slide.deployUrl}</p>
+                  <p className='mt-6'>{slide.repoUrl}</p>
+                </div>
               </>
             )}
           </div>
