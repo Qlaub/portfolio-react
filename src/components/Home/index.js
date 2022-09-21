7dc55576135db63edee1844be7b0db74ea314eec
Alex Glaubitz <e.pirazzi@gmail.com>
2022-09-21 02:35:43 -0400

    updated homepage text color

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,9 +4,9 @@ function Home({linkSelected, setLinkSelected}) {
   return (
     <div className="flex justify-center items-center h-96 bg-transparent">
       <div className="bg-quaternary p-10 mt-8 rounded-3xl h-fit flex flex-col items-center shadow-2xl border border-secondary">
-        <h2 className="text-3xl">Hello! I'm Alex Glaubitz.</h2>
-        <h3 className="text-xl pb-4">I build web applications. </h3>
-        <p className="text-lg max-w-xl border-t border-primary pt-4">
+        <h2 className="text-3xl text-zinc-900">Hello! I'm Alex Glaubitz.</h2>
+        <h3 className="text-xl pb-4 text-zinc-600">I build web applications. </h3>
+        <p className="text-lg text-zinc-700 max-w-xl border-t border-primary pt-4">
           I'm a web developer practiced in the MERN and SERN stacks, and am currently working for a startup company in stealth mode.
           I find joy in details and simplicity.
         </p>
